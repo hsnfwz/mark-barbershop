d2152a4df5b6075a1e6994b61520a46559c9c0c4
Hussein Fawaz <husseinfawaz@MacBook-Pro-7.local>
2024-05-12 12:05:40 -0700

    code completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'raleway': 'Raleway',
+      'raleway-bold': 'Raleway Bold',
+      'roboto': 'Roboto',
+      'roboto-bold': 'Roboto Bold',
+    }
   },
   plugins: [],
 }
